5f469e15ce285564da00811f8e11b479a23dd444
frobiac <frobiac@gmail.com>
2023-03-06 23:20:06 +0100

    [VIAL] BlackBowl: Set unlock combo to upper left keys

--- keyboards/frobiac/blackbowl/keymaps/vial/config.h
+++ keyboards/frobiac/blackbowl/keymaps/vial/config.h
@@ -4,8 +4,8 @@
 // clang-format off
 #define VIAL_KEYBOARD_UID {0xF3, 0x1D, 0x90, 0x95, 0x87, 0x7F, 0xFC, 0x15}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_ROWS { 5, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 0 }
 // clang-format on
 
 #define MAX_LAYER 6
