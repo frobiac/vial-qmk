b8d9d51753c91790b1a13a928ad6add7654f8500
frobiac <frobiac@gmail.com>
2023-03-07 22:58:18 +0100

    [VIAL] blackbowl: Try to fix suspend bug

--- keyboards/frobiac/blackbowl/keymaps/vial/rules.mk
+++ keyboards/frobiac/blackbowl/keymaps/vial/rules.mk
@@ -3,11 +3,6 @@ USER_NAME := frobiac
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-PS2_MOUSE_ENABLE = yes
-PS2_ENABLE = yes
-
-# Upstream QMK changed this to PS2_DRIVER = [busywait|usart|interrupt]
-ifeq ($(strip $(PS2_MOUSE_ENABLE)), yes)
-	PS2_USE_USART = yes
-endif
-
+# try to fix suspend trackpoint bug
+NO_USB_STARTUP_CHECK = yes
+RGB_MATRIX_ENABLE = no
