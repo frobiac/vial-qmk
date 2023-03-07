9dd8efa925366b7481abe8a222532c85c24511f3
frobiac <frobiac@gmail.com>
2023-03-07 22:11:34 +0100

    redtilt: fix PS2_MOUSE, configured by default in userspace rules.mk

--- keyboards/frobiac/redtilt/keymaps/vial/rules.mk
+++ keyboards/frobiac/redtilt/keymaps/vial/rules.mk
@@ -3,12 +3,3 @@ USER_NAME := frobiac
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-PS2_MOUSE_ENABLE = no
-PS2_ENABLE = no
-
-# Upstream QMK changed this to PS2_DRIVER = busywait
-# But only activate once global setting is re-activated
-ifeq ($(strip $(PS2_MOUSE_ENABLE)), yes)
-	PS2_USE_BUSYWAIT = yes
-endif
-
